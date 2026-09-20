ff93e90d55eb7c6dc7fb261e2180db83713b3a6e
davemt74-lang <davemt74@gmail.com>
2026-09-20 11:24:14 -0700

    Phase 22: surface downstream source-change impact in Now

--- app/cognitive-feed.php
+++ app/cognitive-feed.php
@@ -399,6 +399,7 @@ function cognitive_feed_collect_research(PDO $pdo,array $viewer,array &$items): 
     foreach(cognitive_feed_projects($pdo,$viewer,6) as $project)cognitive_feed_collect_project_research($pdo,$viewer,$project,$items);
     if(function_exists('cross_research_ready')&&cross_research_ready($pdo))cross_research_cognitive_observations($pdo,$viewer,$items,14);
     if(function_exists('research_reviews_ready')&&research_reviews_ready($pdo))research_review_cognitive_observations($pdo,$viewer,$items,24);
+    if(function_exists('change_impact_ready')&&change_impact_ready($pdo))change_impact_cognitive_observations($pdo,$viewer,$items,12);
     if(function_exists('research_outcomes_ready')&&research_outcomes_ready($pdo))research_outcome_cognitive_observations($pdo,$viewer,$items,12);
 }
 
