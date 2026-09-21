aa96d2ba549d13b661e7a5f270efbc3bdc4b9725
davemt74-lang <davemt74@gmail.com>
2026-09-21 01:26:17 -0700

    Phase 27: wire Chrome verification entry

--- extension/sidepanel-init.js
+++ extension/sidepanel-init.js
@@ -72,6 +72,7 @@ function initializeSidebarBindings(){
   sidebarBindClick('openPublications',()=>chrome.tabs.create({url:API_BASE+'/research-publications.php'}));
   sidebarBindClick('openNetwork',()=>chrome.tabs.create({url:API_BASE+'/research-network.php'}));
   sidebarBindClick('openAudit',()=>chrome.tabs.create({url:API_BASE+'/research-audit.php'}));
+  sidebarBindClick('openVerification',()=>chrome.tabs.create({url:API_BASE+'/research-verification.php'}));
   sidebarBindClick('openImpact',()=>chrome.tabs.create({url:API_BASE+'/research-impact.php'}));
   sidebarBindClick('openReviews',()=>chrome.tabs.create({url:API_BASE+'/research-reviews.php'}));
   sidebarBindClick('openDecisionMemory',()=>chrome.tabs.create({url:API_BASE+'/research-outcomes.php'}));
