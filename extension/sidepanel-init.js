380ca5a3358d43210a983634a45ed5e5166a3708
davemt74-lang <davemt74@gmail.com>
2026-09-20 21:14:03 -0700

    Phase 24: open Living Research from Chrome

--- extension/sidepanel-init.js
+++ extension/sidepanel-init.js
@@ -69,6 +69,7 @@ function initializeSidebarBindings(){
   sidebarBindClick('markAllNotifications',markAllNotificationsRead);
   sidebarBindClick('openNotificationSettings',()=>chrome.tabs.create({url:API_BASE+'/settings.php#notifications'}));
   sidebarBindClick('openPortfolio',()=>chrome.tabs.create({url:API_BASE+'/research-portfolio.php'}));
+  sidebarBindClick('openPublications',()=>chrome.tabs.create({url:API_BASE+'/research-publications.php'}));
   sidebarBindClick('openImpact',()=>chrome.tabs.create({url:API_BASE+'/research-impact.php'}));
   sidebarBindClick('openReviews',()=>chrome.tabs.create({url:API_BASE+'/research-reviews.php'}));
   sidebarBindClick('openDecisionMemory',()=>chrome.tabs.create({url:API_BASE+'/research-outcomes.php'}));
